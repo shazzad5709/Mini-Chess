840a559dd3c6280d3766fc5a9a99d45e347fb7aa
shazzad5709 <shazzad1111@gmail.com>
2023-09-08 22:54:07 +0600

    merge completed, error fixed

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,9 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 
-// pages/_app.tsx
-
-import { DndProvider } from 'react-dnd';
-import { HTML5Backend } from 'react-dnd-html5-backend';
-
-
 
 export default function App({ Component, pageProps }: AppProps) {
   return <>
-  <DndProvider backend={HTML5Backend}>
       <Component {...pageProps} />
-    </DndProvider>
-  </>
-    
+  </>   
 }
